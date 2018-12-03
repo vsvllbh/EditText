d7f1db1c643eaf4c584f85824c89cc1d2ed4bd77
vsvllbh <vsvllbh143@gmail.com>
2018-12-04 04:45:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vsvll/edittext/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.vsvll.edittext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    //declaration
+    EditText editText;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String name = editText.getText().toString();
+                textView.setText("Hi!!"+name+", how are you today?");
+
+            }
+        });
+
+    }
+}
